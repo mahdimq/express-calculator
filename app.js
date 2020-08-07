616b1c4c3a6ab1daa47d39c8e235bb2f3648b481
Muhammad Qadir <mahdimq@gmail.com>
2020-08-07 22:23:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const ExpressError = require('./expressError')
+const { mean, median, mode } = require('./helper')
+
+const app = express()
+
+// parses body into json object
+app.use(express.json())
+// parses form data into json object
+app.use(express.urlencoded({ extended: true }))
+
+// MEAN ROUTE
+app.get('/mean', (req, res, next) => {
+	if (!req.query.nums) {
+		throw new ExpressError(`${nums} is not a valid number`, 400)
+	}
+	try {
+		let numbers = req.query.nums.split(',')
+
+		const result = {
+			response: {
+				operation: 'mean',
+				value: mean(numbers),
+			},
+		}
+		return res.json(result)
+	} catch (err) {
+		return next(err)
+	}
+})
+
+// MEDIAN ROUTE
+app.get('/median', (req, res, next) => {
+	if (!req.query.nums) {
+		throw new ExpressError(`${nums} is not a valid number`, 400)
+	}
+	try {
+		let numbers = req.query.nums.split(',')
+
+		const result = {
+			response: {
+				operation: 'median',
+				value: median(numbers),
+			},
+		}
+		return res.json(result)
+	} catch (err) {
+		return next(err)
+	}
+})
+
+// MODE ROUTE
+app.get('/mode', (req, res, next) => {
+	if (!req.query.nums) {
+		throw new ExpressError(`${nums} is not a valid number`, 400)
+	}
+	try {
+		let numbers = req.query.nums.split(',')
+
+		const result = {
+			response: {
+				operation: 'mode',
+				value: mode(numbers),
+			},
+		}
+		return res.json(result)
+	} catch (err) {
+		return next(err)
+	}
+})
+
+// ERROR HANDLER
+app.use((error, req, res, next) => {
+	// the default status is 500 Internal Server Error
+	let status = error.status || 500
+	let message = error.message
+
+	// set the status and alert the user
+	return res.status(status).json({ error: { message, status } })
+})
+
+app.listen(3000, () => console.log('Server started on port 3000'))
---
+++ helper.js
@@ -0,0 +1,46 @@
+// Calculate Mean
+const mean = (arr) => arr.reduce((acc, next) => acc + next, 0) / arr.length
+
+// Calculate Median
+const median = (arr) => {
+	const midNum = Math.floor(arr.length / 2)
+	arr.sort((a, b) => a - b)
+	if (arr.length % 2) {
+		return arr(midNum)
+	} else {
+		return (arr[midNum - 1] + arr[midNum]) / 2
+	}
+}
+
+// Calculate Mode
+const mode = (arr) => {
+	let count = {}
+	let maxCount = 0
+	let num
+	for (let i = 0; i < arr.length; i++) {
+		if (!count[arr[i]]) {
+			count[arr[i]] = 0
+		}
+		count[arr[i]]++
+	}
+	for (num in count) {
+		if (count[num] > maxCount) {
+			maxCount = num
+		}
+	}
+	return maxCount
+}
+
+// const checkError = (req, res, next) => {
+// 	try {
+// 		if (!req.query.nums) {
+// 			throw new ExpressError(`${nums} is not a valid number`, 400)
+// 		} else {
+// 			next()
+// 		}
+// 	} catch (err) {
+// 		return next(err)
+// 	}
+// }
+
+module.exports = { mean, median, mode, checkError }
